b466431f1b1eac055ea8d23f17156c049c667f7d
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-10-28 17:04:00 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './styles.css';
+import {
+  alert,
+  defaultModules,
+} from "../node_modules/@pnotify/core/dist/PNotify.js";
+import * as PNotifyMobile from "../node_modules/@pnotify/mobile/dist/PNotifyMobile.js";
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+import "../node_modules/@pnotify/mobile/dist/PNotifyMobile.css";
+import "../node_modules/@pnotify/core/dist/BrightTheme.css";
+
+import css from "./css/style.css";
+
+defaultModules.set(PNotifyMobile, {});
+alert("Notice me, senpai!");
