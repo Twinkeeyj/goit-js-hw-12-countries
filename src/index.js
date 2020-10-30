c55d11fec5364e06f35c751d54226c4af81063fe
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-10-30 22:57:13 +0200

    робочий варіант

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import {
-  alert,
-    defaultModules,
-  info
-} from "../node_modules/@pnotify/core/dist/PNotify.js";
-import * as PNotifyDesktop from "../node_modules/@pnotify/desktop/dist/PNotifyDesktop.js";
-import "../node_modules/@pnotify/core/dist/PNotify.css";
-import "../node_modules/@pnotify/desktop/dist/PNotifyDesktop.css";
-import '../node_modules/@pnotify/core/dist/BrightTheme.css'
+
 import "./styles.css"
 // import template from "./template/listTemplate.hbs"
-defaultModules.set(PNotifyDesktop, {});
+// defaultModules.set(PNotifyDesktop, {});
 
 import fetchCountries from "./components/fetchCountries.js"
 
