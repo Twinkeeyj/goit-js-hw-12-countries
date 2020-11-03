db194cd81f78daef9448cdf43f0f0ae493bf5e29
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-11-03 16:09:47 +0200

    1

--- src/components/fetchCountries.js
+++ src/components/fetchCountries.js
@@ -1,82 +1,10 @@
-import template from "../template/listTemplate.hbs";
-import {alert, info} from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import '@pnotify/core/dist/BrightTheme.css'
 
 
 export default function fetchCountries(searchQuery) {
-   const input = document.querySelector('.input')
-   const ul = document.querySelector('.containerList')
-  const debounce = require("lodash.debounce");
-  const minionss = document.querySelector('.minions')
-  const thoughts = document.querySelector('.thoughts')
-  const minions2 = document.querySelector('.minions2')
-  
-  
-    const countries = function (e) {
-      if ( e.target.value.length >= 1) {
-                    fetch(`https://restcountries.eu/rest/v2/name/${e.target.value}`)
+ return  fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
                 .then((data) => data.json())
-                .then((array) => {
-
-                   if (array.length > 10) {
-                     alert("      Too many matches found.Please \n enter a more specific query!!!!")
-                  };
-
-                  if (array.length >= 0) {
-                    ul.innerHTML = ""
-                  };
-                  array.forEach((el) => {
-                     if (array.length >= 2 && array.length < 10) {
-                       ul.innerHTML += `<li class=listItem>${el.name}</li>`
-                       const listItem = document.querySelectorAll('.listItem')
-                       listItem.forEach((el) => {
-
-                        const replacement = function () {
-                         input.value = el.textContent
-                         ul.innerHTML = ''
-                         fetch(`https://restcountries.eu/rest/v2/name/${el.textContent}`)
-                         .then((data) => data.json())
-                         .then((array) => { ul.insertAdjacentHTML('beforeend', template(array))});
-                        }
-                            el.addEventListener('click',replacement)
-                       })
-                    
-                        } else if (array.length === 1) {
-                       ul.insertAdjacentHTML('beforeend', template(array));
-                      //  localStorage.setItem('html', template(array));
-
-                       
-                     }
-                    })
-                })
-                 .catch(a => {
-                   input.value = ""
-                   ul.innerHTML = ""
-                    info('     Enter another name!')
-                 })
-        
-        
-        
-      }else{ul.innerHTML = ""}
-
-  };
+                .then((array) => array)
  
-
-  const minions = function () {
-    minionss.classList.toggle('invisible')
-    thoughts.classList.toggle('invisible')
-    minions2.classList.toggle('invisible')
-    input.removeEventListener("click", minions)
-  }
-
-   input.addEventListener("click", minions)
-   document.querySelector('input').addEventListener(
-  'input',
-    debounce((e) => {
-      countries(e)}, 500),
-  );
-    
 }
 
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,82 @@
 import "./styles.css"
 import fetchCountries from "./components/fetchCountries.js"
 
+import template from "./template/listTemplate.hbs";
+import {alert, info} from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css'
 
-// document
-//     .querySelector('.containerList')
-//     .insertAdjacentHTML('beforeend', localStorage.getItem('html' || '' );
+ const input = document.querySelector('.input')
+   const ul = document.querySelector('.containerList')
+  const debounce = require("lodash.debounce");
+  const minionss = document.querySelector('.minions')
+  const thoughts = document.querySelector('.thoughts')
+  const minions2 = document.querySelector('.minions2')
 
-fetchCountries()
+
+   const countries = function (e) {
+      if ( e.target.value.length >= 1) {
+                 fetchCountries(e.target.value)
+                .then((array) => {
+
+                   if (array.length > 10) {
+                     alert("      Too many matches found.Please \n enter a more specific query!!!!")
+                  };
+
+                  if (array.length >= 0) {
+                    ul.innerHTML = ""
+                  };
+                  array.forEach((el) => {
+                     if (array.length >= 2 && array.length < 10) {
+                         ul.innerHTML += `<li class=listItem>${el.name}</li>`
+                         
+
+                        //  const listItem = document.querySelectorAll('.listItem')
+  
+                    //    listItem.forEach((list) => {
+                    //     const replacement = function () {
+                    //      input.value = list.textContent
+                    //         ul.innerHTML = ''
+                            
+                    //      fetchCountries(e.target.value)
+                    //          .then((array) => {
+                    //              ul.insertAdjacentHTML('beforeend', template(array))
+                    //          });
+                    //     }
+                    //         list.addEventListener('click',replacement)
+                    //    })
+               
+                        } else if (array.length === 1) {
+                       ul.insertAdjacentHTML('beforeend', template(array));
+                     
+
+                       
+                     }
+                    })
+                })
+                 .catch(a => {
+                   input.value = ""
+                   ul.innerHTML = ""
+                    info('     Enter another name!')
+                 })
+        
+        
+        
+      }else{ul.innerHTML = ""}
+
+  };
+ 
+
+  const minions = function () {
+    minionss.classList.toggle('invisible')
+    thoughts.classList.toggle('invisible')
+    minions2.classList.toggle('invisible')
+    input.removeEventListener("click", minions)
+  }
+
+   input.addEventListener("click", minions)
+   document.querySelector('input').addEventListener(
+  'input',
+    debounce((e) => {
+      countries(e)}, 500),
+  );
